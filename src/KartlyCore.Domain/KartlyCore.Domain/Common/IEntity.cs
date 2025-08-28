b87f72faa5b3408edf5ec0597087bb88595ea0cc
bishalsarker <mr.bishalsarker@gmail.com>
2025-08-28 13:36:53 +0600

    init repo

---
+++ src/KartlyCore.Domain/KartlyCore.Domain/Common/IEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace FlyerBuy.Domain.Common
+{
+    public interface IEntity
+    {
+        object Id { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedOnUtc { get; set; }
+        public string LastModifiedBy { get; set; }
+        public DateTime? LastModifiedOnUtc { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
